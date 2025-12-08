6279fccba5f853cb160597719cceeed0fbb17098
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-08 13:48:52 +0330

    prime_maker machine got better

--- taklif/prime_maker.c
+++ taklif/prime_maker.c
@@ -7,11 +7,6 @@ int main() {
     int primeChecker = 0;
     if (input <= 1) {
         printf("no prime numbers ");
-        return 1;
-    }
-    if (input == 2) {
-        printf("2");
-        return 2;
     }
     while (primeMaker <= input) {
         for (int i = 2; i < primeMaker; i++) {
