508149bb79631330507a295da29ad4d281fafbb8
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-19 08:35:45 +0330

    bubble sort of array first version

---
+++ taklif/array_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arraySize;
+    int temp =0;
+    printf("give me the size of your array : \n");
+    scanf(" %d",&arraySize);
+    int myArray[arraySize];
+    for (int i = 0;i<arraySize;i++) {
+        printf("%d element is : \n",i+1);
+        scanf(" %d",&myArray[i]);
+    }
+    for (int i =0;i<arraySize;i++) {
+        for (int j = 0;j<arraySize;j++) {
+            if (myArray[j]<myArray[j+1]) {
+                temp = myArray[j+1];
+                myArray[j+1]=myArray[j];
+                myArray[j]=temp;
+            }
+        }
+    }
+    for (int i = 0;i<arraySize;i++) {
+        printf("%d\n",myArray[i]);
+    }
+    return 0;
+}
