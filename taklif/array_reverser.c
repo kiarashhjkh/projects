a906261d0db5e925e2bdc21cdad3047747c65aa0
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-19 08:58:09 +0330

    gives you the elements of your array in reverse order

---
+++ taklif/array_reverser.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int arraySize;
+    printf("give me the size of your array : \n");
+    scanf(" %d", &arraySize);
+    int myArray[arraySize];
+    for (int i = 0; i < arraySize; i++) {
+        printf("%d element is : \n", i + 1);
+        scanf(" %d", &myArray[i]);
+    }
+    for (int i = 0; i < arraySize; i++) {
+        printf("%d  ", myArray[i]);
+        if (i == arraySize - 1) {
+            printf("\n");
+        }
+    }
+    for (int i = arraySize - 1;i >= 0;i--) {
+        printf("%d  ",myArray[i]);
+    }
+        return 0;
+}
