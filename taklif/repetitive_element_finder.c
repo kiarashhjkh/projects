f31a4f6f1d90afc6280debe968248a4bf827e058
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-19 12:42:48 +0330

    added a text that if there are no duplicates it says so

--- taklif/repetitive_element_finder.c
+++ taklif/repetitive_element_finder.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int arraySize;
+    int arraySize, dupCheck = 0;
     printf("give me the size of your array : \n");
     scanf(" %d", &arraySize);
     int myArray[arraySize];
@@ -9,13 +9,15 @@ int main() {
         printf("%d element is : \n", i + 1);
         scanf(" %d", &myArray[i]);
     }
-    for (int i = 0;i<arraySize;i++) {
-        for (int j=i+1;j<arraySize;j++) {
-            if (myArray[i]==myArray[j]) {
-                printf("%d ",myArray[i]);
+    for (int i = 0; i < arraySize; i++) {
+        for (int j = i + 1; j < arraySize; j++) {
+            if (myArray[i] == myArray[j]) {
+                printf("%d ", myArray[i]);
+                dupCheck++;
                 break;
             }
         }
     }
+    if (dupCheck == 0) { printf("no duplicates"); }
     return 0;
 }
