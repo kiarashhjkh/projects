5998e7e6d0d69addcc30f08bab95aff7ccf70a4f
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-19 11:38:25 +0330

    this code uses the number of occurrences digits and stores them in a array with 10 elements

---
+++ taklif/numbers_with_same_digits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int input1, input2,commonCount=0;
+    printf("give me your first number :");
+    scanf(" %d", &input1);
+    printf("give me your second number :");
+    scanf(" %d", &input2);
+    int freq1[10]={0}, freq2[10]={0};
+    while (input1 > 0) {
+        freq1[input1 % 10]++;
+        input1 /= 10;
+    }
+    while (input2 > 0) {
+        freq2[input2 % 10]++;
+        input2 /= 10;
+    }
+    for (int i =0; i<10;i++) {
+        if (freq1[i]<freq2[i]) {
+            commonCount+=freq1[i];
+        }else{commonCount+=freq2[i];}
+    }
+    printf("%d",commonCount);
+    return 0;
+}
