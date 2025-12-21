9e3e9e77cac337994d5538d978dc007abca53997
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-21 22:17:59 +0330

    it now stores the nonzero elements in an array of its own

--- taklif/matrix_optimal_showcase.c
+++ taklif/matrix_optimal_showcase.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    int row, column, zeroCounter = 0;
+    int row, column, nonZeroCounter = 0;
     printf("number of rows :");
     scanf(" %d", &row);
     printf("number of columns :");
     scanf(" %d", &column);
     int matrix[row][column];
-    int rowHolder[row * column], columnHolder[row * column], nonZeroHolder[row * column];
     printf("enter the elements of the matrix : \n");
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
             printf("[%d][%d] :", i, j);
             scanf(" %d", &matrix[i][j]);
             if (matrix[i][j] != 0) {
-                rowHolder[zeroCounter] = i;
-                columnHolder[zeroCounter] = j;
-                nonZeroHolder[zeroCounter] = matrix[i][j];
-                zeroCounter++;
+                nonZeroCounter++;
             }
         }
     }
-    printf("%d %d %d\n", row, column, zeroCounter);
-    for (int i = 0 ; i < zeroCounter;i++) {
-        printf("%d %d %d \n",rowHolder[i],columnHolder[i],nonZeroHolder[i]);
+    int nonZeroHolder[nonZeroCounter];
+    int temp = 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (matrix[i][j] != 0) {
+                nonZeroHolder[temp]=matrix[i][j];
+                temp++;
+            }
+        }
+    }
+    temp = 0;
+    printf("%d %d %d \n",row,column,nonZeroCounter);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            if (matrix[i][j] != 0) {
+                printf("%d %d %d\n",i,j,nonZeroHolder[temp]);
+                temp++;
+            }
+        }
     }
     return 0;
 }
