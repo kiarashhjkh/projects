2395a0bc8e6e7ac9875cd63ef61c2a9d11fe2c76
Kiarash <kiarash.hj.kh@gmail.com>
2025-12-21 20:34:05 +0330

    it now stores the nonzero elements in an array of its own and the position they are in

--- taklif/matrix_optimal_showcase.c
+++ taklif/matrix_optimal_showcase.c
@@ -7,23 +7,23 @@ int main() {
     printf("number of columns :");
     scanf(" %d", &column);
     int matrix[row][column];
+    int rowHolder[row * column], columnHolder[row * column], nonZeroHolder[row * column];
     printf("enter the elements of the matrix : \n");
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++) {
             printf("[%d][%d] :", i, j);
             scanf(" %d", &matrix[i][j]);
             if (matrix[i][j] != 0) {
+                rowHolder[zeroCounter] = i;
+                columnHolder[zeroCounter] = j;
+                nonZeroHolder[zeroCounter] = matrix[i][j];
                 zeroCounter++;
             }
         }
     }
     printf("%d %d %d\n", row, column, zeroCounter);
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < column; j++) {
-            if (matrix[i][j] != 0) {
-                printf("%d %d %d\n", i, j, matrix[i][j]);
-            }
-        }
+    for (int i = 0 ; i < zeroCounter;i++) {
+        printf("%d %d %d \n",rowHolder[i],columnHolder[i],nonZeroHolder[i]);
     }
     return 0;
 }
